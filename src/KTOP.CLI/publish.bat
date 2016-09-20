34baf2a22446c2647d827a08e7490ff364a04c1a
Ali Bahraminezhad <ali.bhz@gmail.com>
2016-09-20 13:05:00 +0430

    update publish bat, sh

--- src/KTOP.CLI/publish.bat
+++ src/KTOP.CLI/publish.bat
@@ -1,6 +1,3 @@
 dotnet restore
-dotnet publish -r debian.8-x64
-dotnet publish -r ubuntu.14.04-x64
-dotnet publish -r ubuntu.16.04-x64
-dotnet publish -r win7-x64
-dotnet publish -r osx.10.10-x64
+dotnet build
+dotnet publish
